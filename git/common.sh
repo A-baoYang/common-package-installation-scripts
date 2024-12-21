eefee5ae9cbb6ff07c2d8b89ed6c79f904ca21ec
A-baoYang <rubyyang0615@gmail.com>
2024-12-21 16:21:35 +0800

    Add git/common.sh

---
+++ git/common.sh
@@ -0,0 +1,11 @@
+# tag a commit
+git tag -a <tagname> -m "message"
+
+# tag a branch
+git tag <tagname>
+
+# push tag to remote
+git push origin <tagname> --tags
+
+# delete specific tag
+git tag --delete <tagname>
