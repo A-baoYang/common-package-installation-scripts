d00d27d9377244011114dd55bef1e782dcd0c0f8
A-baoYang <rubyyang0615@gmail>
2025-03-03 16:10:20 +0800

    Update docker/common.sh

--- docker/common.sh
+++ docker/common.sh
@@ -3,3 +3,6 @@ docker rmi $(docker images -q -f "dangling=true")
 
 # copy file from container
 docker cp CONTAINER:/var/logs/ /tmp/app_logs
+
+# clean docker images except tagged "latest" one
+docker images --format "{{.Repository}}:{{.Tag}}" | grep -v "latest" | xargs -r docker rmi
